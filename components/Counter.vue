5374f93e2ebaae7237a4f2d06861889f376fd7d5
Mark Neil <mark.neil.u.cordero@gmail.com>
2025-01-06 19:00:26 +0800

    Add Easy Challenge Number 1

---
+++ components/Counter.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="counter-container">
+        <h2>Counter: {{ count }}</h2>
+        <button @click="increment">Increment</button>
+        <button @click="decrement" :disabled="count === 0">Decrement</button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+
+const increment = () => {
+    count.value++;
+};
+
+const decrement = () => {
+    if (count.value > 0)
+    {
+        count.value--;
+    }
+};
+</script>
+
+<style>
+.counter-container {
+    text-align: center;
+    padding: 1rem;
+}
+
+button {
+    margin: 0.5rem;
+    padding: 0.5rem 1rem;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>Welcome to the Nuxt App!</h1>
+        <Counter />
+    </div>
+</template>
+
+<script setup>
+import Counter from '@/components/Counter.vue';
+</script>
