e33ef9d808061ae59fe416a0f63e6a0e999bcfd2
Mark Neil <mark.neil.u.cordero@gmail.com>
2025-01-21 23:05:29 +0800

    Add ReusableButton.vue

---
+++ components/ReusableButton.vue
@@ -0,0 +1,42 @@
+<template>
+    <button
+        :type="type"
+        class="btn"
+        @click="onClick"
+    >
+        {{ label }}
+    </button>
+</template>
+
+<script setup>
+defineProps({
+    label: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        default: 'button',
+    },
+    onClick: {
+        type: Function,
+        required: false,
+    },
+});
+</script>
+
+<style scoped>
+.btn {
+    background-color: #4CAF50;
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 16px;
+}
+
+.btn:hover {
+    background-color: #45a049;
+}
+</style>
